0ad793d7a21d28293d36b73047d63e1aa3d6efda
Miguel ferreira sena <166344275+ougwYT@users.noreply.github.com>
2025-01-21 09:03:06 -0300

    Add files via upload

---
+++ projeto dev club/projeto dev club/script.js
@@ -0,0 +1,11 @@
+let imagem = document.querySelector(".troca-imagem");
+
+function trocaimagem() {
+  if (imagem.style.opacity == 0) {
+    imagem.style.opacity = 1;
+  } else {
+    imagem.style.opacity = 0;
+  }
+}
+
+setInterval(trocaimagem, 3000);
